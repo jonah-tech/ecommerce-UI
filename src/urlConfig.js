6983aad5b922d27a2f812b2a3f87063bccbf9113
jonah-tech <kiharajoanh@gmail.com>
2022-01-14 21:51:24 +0300

    ulrconfig changed

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,6 +1,7 @@
-const baseUrl = "http://localhost:2000";
+const baseUrl = process.env.API || "https://ecommerce-rest-server-app.herokuapp.com";
 
 export const api = `${baseUrl}/api`;
+
 export const generatePublicUrl = (fileName) => {
   return `${baseUrl}/public/${fileName}`;
 };
